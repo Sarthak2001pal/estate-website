4a42471b0a9ef99911d8613a7bf895830122ea48
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-10 14:34:17 +0530

    api server

--- estate-new/src/components/Header.jsx
+++ estate-new/src/components/Header.jsx
@@ -22,9 +22,11 @@ export default function Header() {
           <Link to='/'>
           <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
+          
           <Link to='/about'>
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
+
           <Link to='/sign-in'>
           <li className='sm:inline text-slate-700 hover:underline'>Sign in</li>
           </Link>
