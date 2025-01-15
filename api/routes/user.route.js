a63f3f709b62dfadbab33a67a18342610a3ed6d7
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-15 21:21:43 +0530

    implement user information update functionality with password hashing and token verification added user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUserInfo } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();    // Create a new router
 router.get('/test', test);    // Create a new route with the path /test
+router.post('/update/:id', verifyToken ,updateUserInfo);    // Create a new route with the path /update/:id
 export default router;    // Export the router
