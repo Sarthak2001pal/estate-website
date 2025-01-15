81419742041f7c865f34ef9a8bd1044f75a9ea91
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-15 11:16:56 +0530

    enhance profile page with user avatar display and update form fields

--- estate-new/src/pages/Profile.jsx
+++ estate-new/src/pages/Profile.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const {currentUser} = useSelector(state => state.user);
   return (
-    <div>Profile</div>
+    <div className='max-w-lg mx-auto p-3'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img src={currentUser.avatar} alt="Profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center' />
+        <input type="text" placeholder='username' id='username'className='border p-3 rounded-lg'/>
+        <input type="email" placeholder='email' id='email'className='border p-3 rounded-lg'/>
+        <input type="text" placeholder='password' id='password'className='border p-3 rounded-lg'/>
+        <button className='bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-500 cursor-pointer hover:text-red-800'>Delete account</span>
+        <span className='text-red-500 cursor-pointer hover:text-red-800'>Sign out</span>
+      </div>
+    </div>
   )
 }
