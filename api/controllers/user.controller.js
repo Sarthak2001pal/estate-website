efd7673421ce67014851a26e80e87851290175a8
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-17 16:13:56 +0530

    completed show user listing functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
+import Listings from '../models/listing.model.js';
 
 export const test = (req, res) => {
     res.json({ message: 'śenoorita !' });
@@ -41,15 +42,15 @@ export const deleteUserInfo = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
 
 export const getUserListings = async(req, res,next) =>{
     if(req.user.id ===req.params.id) {
         try {
-            const Listings = await Listing.find({ userRef: req.params.id });
-            res.status(200).json(Listings);
+            const listings = await Listings.find({ userRef: req.params.id });
+            res.status(200).json(listings);
         } catch (error) {
-            next(error)
+            next(error);
         }
     } else {
         next(errorHandler(401, 'Alter your own account'));
