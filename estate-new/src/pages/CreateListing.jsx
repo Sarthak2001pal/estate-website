505a1570308089ba0416626c4986828b8ac63501
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-18 15:38:33 +0530

    add rental price display logic and share functionality in listing pages completed listing page

--- estate-new/src/pages/CreateListing.jsx
+++ estate-new/src/pages/CreateListing.jsx
@@ -201,7 +201,9 @@ export default function CreateListing() {
                   <input onChange={handleChange} value={formData.regularPrice} type="number" id='regularPrice' min='50' required className='p-3 border border-gray-300 rounded-lg' />
                   <div className='flex flex-col items-center'>
                     <p>Regular Price</p>
-                    <span className='text-xs'>($ / month)</span>
+                    {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                    )}
                   </div>                  
                 </div>
                 {
@@ -210,7 +212,9 @@ export default function CreateListing() {
                       <input onChange={handleChange} value={formData.discountPrice} type="number" id='discountPrice' min='0' required className='p-3 border border-gray-300 rounded-lg' />
                         <div className='flex flex-col items-center'>
                           <p>Discounted Price</p>
-                          <span className='text-xs'>($ / month)</span>
+                          {formData.type === 'rent' && (
+                            <span className='text-xs'>($ / month)</span>
+                          )}
                         </div>
                     </div>
                   )
