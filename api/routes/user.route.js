2bbecf8382e48afd2d51755fef4181a89eab19aa
Sarthak2001pal <79644703+Sarthak2001pal@users.noreply.github.com>
2025-01-18 17:38:43 +0530

    add contact landlordfunctionality to listing page and user retrieval endpoint

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUserInfo, test, updateUserInfo, getUserListings } from '../controllers/user.controller.js';
+import { deleteUserInfo, test, updateUserInfo, getUserListings, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();   
@@ -7,4 +7,5 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken ,updateUserInfo);  
 router.delete('/delete/:id', verifyToken ,deleteUserInfo); 
 router.get('/listings/:id', verifyToken ,getUserListings);
+router.get('/:id', verifyToken, getUser)
 export default router;    
